c844752830fb9d850a3a43dfc4af66e1b92078cb
Hang Shao <shaohang1994@163.com>
2025-07-17 14:47:35 +0800

    Add files via upload

---
+++ install.Runiversal.r
@@ -0,0 +1,18 @@
+install.packages("remotes")
+install.packages("devtools")
+devtools::install_local("Runiversal-master.zip")
+
+将程序包安装入‘C:/Users/shaoh/AppData/Local/R/win-library/4.3’
+(因为‘lib’没有被指定)
+* installing *source* package 'Runiversal' ...
+** package 'Runiversal' successfully unpacked and MD5 sums checked
+** using staged installation
+** R
+** byte-compile and prepare package for lazy loading
+** help
+*** installing help indices
+** building package indices
+** testing if installed package can be loaded from temporary location
+** testing if installed package can be loaded from final location
+** testing if installed package keeps a record of temporary installation path
+* DONE (Runiversal)
